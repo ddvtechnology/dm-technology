4820d9de36c130b89659ae4fc47c8f4fa7da16df
ddvtechnology <ddvtechnology@gmail.com>
2026-05-25 18:04:34 -0300

    --

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/webp", "image/avif"],
-  },
-  turbopack: {
-    root: path.resolve(process.cwd()),
   },
 };
 
